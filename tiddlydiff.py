4af8fc526f98ca7ec919b9dcc68828adcda7f2fc
aj heller <aj@drfloob.com>
2011-10-12 01:02:31 -0700

    parses and uses command line options; adds the ability to restore previous versions

--- tiddlydiff.py
+++ tiddlydiff.py
@@ -19,7 +19,7 @@ __license__ = """
 
 
 ################################################################################
-###  CONFIG
+###  DEFAULT CONFIG - these can all be overriden by command line options
 
 # the base name of your wiki file. My file is "wiki.html"
 TIDDLY_NAME="wiki"
@@ -32,26 +32,50 @@ BKUP_DIR="bkup"
 DIFF_DIR="bkup.diffs"
 
 
-###  END OF CONFIG
+###  END CONFIG
 ################################################################################
 
 
 
 from os import mkdir, listdir, system, remove
-from os.path import isdir, isfile, join
+from os.path import isdir, isfile, join, basename
 from pprint import pprint
+import shutil
+
+
+from optparse import OptionParser, OptionGroup # python 2.3 to 2.6
+
+parser = OptionParser("%prog command [config] [options]")
+commandGroup = OptionGroup(parser, "Commands")
+commandGroup.add_option("-b", "--backup", action="store_true", dest="doBackup",
+                  help="convert your backups to diffs")
+commandGroup.add_option("-r", "--restore", dest="restoreFile", 
+                  help="restore a version of your wiki from backup diffs; takes the name (with optional path) of the diff to be restored; creates a new html file in your backup diffs folder.")
+parser.add_option_group(commandGroup)
+
+configGroup = OptionGroup(parser, "Config", "folder and various config options")
+configGroup.add_option("-n", "--name", dest="tiddlyName", default=TIDDLY_NAME,
+                       help="name of the wiki file (e.g. 'wiki' for wiki.html)")
+configGroup.add_option("-k", "--bkdir", dest="backupDir", default=BKUP_DIR,
+                       help="path to normal tiddlywiki backup folder")
+configGroup.add_option("-f", "--diffdir", dest="diffDir", default=DIFF_DIR,
+                       help="path to backup diff destination folder")
+parser.add_option_group(configGroup)
 
 
 def make_backup():
 
-    backup_files = sorted(filter(lambda x: x.startswith(TIDDLY_NAME) and x.endswith(".html"), listdir(BKUP_DIR)))
-    #print backup_files
+    tn = options.tiddlyName
+    bd = options.backupDir
+    dd = options.diffDir
 
-    if not isdir(DIFF_DIR):
-        mkdir(DIFF_DIR)
+    backup_files = sorted(filter(lambda x: x.startswith(tn) and x.endswith(".html"), listdir(bd)))
+
+    if not isdir(dd):
+        mkdir(dd)
     else:
         # dir existed. if an html file exists, append it to the backup_files list
-        diffbasefiles = sorted(filter(lambda x: x.startswith(TIDDLY_NAME) and x.endswith(".html"), listdir(DIFF_DIR)))
+        diffbasefiles = sorted(filter(lambda x: x.startswith(tn) and x.endswith(".html"), listdir(dd)))
         diffbasefile = max(diffbasefiles)
 
 
@@ -65,36 +89,92 @@ def make_backup():
         # all is well, perform the diff
         system("diff -u " + fn1 + " " + fn2 + " > " + outfile)
         
+    # @TODO: smarter filtering: just diff the files that haven't been diffed yet, based on the diff base file
+
     ### Diff the diffbasefile with the oldest tiddlywiki backup
     if diffbasefile != "":
         print "Diffing previous base file against oldest tiddlywiki backup"
-        outfile = join(DIFF_DIR, diffbasefile) + ".diff"
-        fn1 = join(BKUP_DIR, min(backup_files))
-        fn2 = join(DIFF_DIR, diffbasefile)
+        outfile = join(dd, diffbasefile) + ".diff"
+        fn1 = join(bd, min(backup_files))
+        fn2 = join(dd, diffbasefile)
         doDiff(fn1, fn2, outfile)
 
 
     ### Do the rest of the diffs
     for i in xrange(len(backup_files)-2, -1, -1):
         print len(backup_files) - i - 1, "of", len(backup_files)-1
-        outfile = join(DIFF_DIR, backup_files[i]) + ".diff"
-        fn1 = join(BKUP_DIR, backup_files[i+1])
-        fn2 = join(BKUP_DIR, backup_files[i])
+        outfile = join(dd, backup_files[i]) + ".diff"
+        fn1 = join(bd, backup_files[i+1])
+        fn2 = join(bd, backup_files[i])
         doDiff(fn1, fn2, outfile)
 
 
 
     ### Copy the most recent backup file, on which the diff chain is based
-    system("cp " + join(BKUP_DIR, max(backup_files)) + " " + DIFF_DIR)
+    system("cp " + join(bd, max(backup_files)) + " " + dd)
 
     ### Remove all other diff base files
     print "Cleaning up old base files"
     if diffbasefiles is not None:
         for i in diffbasefiles:
-            remove(join(DIFF_DIR, i))
+            remove(join(dd, i))
 
     print "DONE."
 
 
+
+def restore_file():
+    # make sure file exists in diffs
+
+    rf = basename(options.restoreFile)
+    if rf.endswith(".diff"):
+        rf = rf[0:-5]
+    if not rf.endswith(".html"):
+        print("invalid file")
+        exit(1)
+    rfd = rf + ".diff"
+
+    dd = options.diffDir
+    if not isfile(join(dd, rfd)):
+        print("file not found")
+        exit(1)
+
+    # copy base diff file with desired name
+    diffs = sorted(listdir(dd))
+    diffbasefiles = filter(lambda x: x.startswith(options.tiddlyName) and x.endswith(".html"), diffs)
+    diffbasefile = max(diffbasefiles)
+    pdbf = join(dd, diffbasefile)
+    prf = join(dd, rf)
+    shutil.copyfile(pdbf, prf)
+
+    # filter diffs to only those we want to apply
+    keepdiffs = filter(lambda x: x > rfd and x.endswith(".diff"), diffs)
+
+    # apply in order, newest to oldest
+    print "len", len(keepdiffs)
+    for i in xrange(len(keepdiffs)-1,-1,-1):
+        ppf = join(dd, keepdiffs[i])
+        system(" ".join(["patch", "-u", prf, ppf]))
+
+    print "DONE"
+
+def parserError(str):
+    print "ERROR:", str, "\n"
+    parser.print_help()
+    exit(1)
+
 if __name__ == "__main__":
-    make_backup()
+    (options, args) = parser.parse_args()
+
+    if len(args) > 0:
+        parserError("couldn't parse arguments; incorrect usage")
+    if options.doBackup and options.restoreFile:
+        parserError("you can only run one command at a time")
+
+    if options.doBackup:
+        make_backup()
+    elif options.restoreFile:
+        restore_file()
+    else:
+        parserError("no command given")
+    
